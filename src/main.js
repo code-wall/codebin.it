cda1662876eac063db45fa75355b80b1b1d48fd1
Alec Hewitt <alec.hewitt9@gmail.com>
2015-11-29 19:58:01 +0000

    Remove unneccessary log statements

--- src/main.js
+++ src/main.js
@@ -15,8 +15,6 @@ class Main {
     }
 
     saveClicked(event) {
-        console.log("Save button Clicked");
-        console.log(this.codeEditor.getValue());
         let snippet = this.codeEditor.getValue();
         this.xmlPost("/save-snippet", {language: "javascript", snippet: snippet})
             .then(function() {
@@ -30,21 +28,16 @@ class Main {
         return new Promise(function(resolve, reject) {
             let xhttp = new XMLHttpRequest();
             xhttp.onload = function() {
-                console.log("xhttp ready state: ", xhttp.readyState);
-                console.log("status: ", xhttp.status);
                 if (xhttp.readyState == 4 && xhttp.status == 200) {
-                    console.log("Response: ", xhttp.responseText);
                     resolve();
                 }
             };
             xhttp.onerror = function(err) {
-                console.error("Error in request");
-                reject()
+                reject(err)
             };
             xhttp.open("POST", url, true);
             xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
             let queryStr = Object.keys(params).reduce(function(a,k){a.push(k+'='+encodeURIComponent(params[k]));return a},[]).join('&');
-            console.log(queryStr);
             xhttp.send(queryStr);
         });
 
