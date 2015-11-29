1f782542c48a8c9ecdba4107bbc985f7fa2e1e99
Alec Hewitt <alec.hewitt9@gmail.com>
2015-11-29 18:12:40 +0000

    Saving snippets working

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/alechewitt/code-wall/app/db"
 	"github.com/gorilla/mux"
 	"mime"
--- src/main.js
+++ src/main.js
@@ -2,12 +2,52 @@
 
 class Main {
     constructor() {
-        let mainTextArea = document.getElementById("mainTextArea");
+        this.saveButton = document.getElementById("saveButton");
+        this.saveButton.addEventListener("click", this.saveClicked.bind(this), false);
+
+        // Init the text area
+        let textArea = document.getElementById("mainTextArea");
         let codeMirrorOpts = {
             lineNumbers: true,
             theme      : "solarized dark"
         };
-        let myCodeMirror = CodeMirror.fromTextArea(mainTextArea, codeMirrorOpts);
+        this.codeEditor = CodeMirror.fromTextArea(textArea, codeMirrorOpts);
+    }
+
+    saveClicked(event) {
+        console.log("Save button Clicked");
+        console.log(this.codeEditor.getValue());
+        let snippet = this.codeEditor.getValue();
+        this.xmlPost("/save-snippet", {language: "javascript", snippet: snippet})
+            .then(function() {
+                console.log("Success");
+            }).catch(function(err) {
+                console.error("Error");
+            })
+    }
+
+    xmlPost(url, params) {
+        return new Promise(function(resolve, reject) {
+            let xhttp = new XMLHttpRequest();
+            xhttp.onload = function() {
+                console.log("xhttp ready state: ", xhttp.readyState);
+                console.log("status: ", xhttp.status);
+                if (xhttp.readyState == 4 && xhttp.status == 200) {
+                    console.log("Response: ", xhttp.responseText);
+                    resolve();
+                }
+            };
+            xhttp.onerror = function(err) {
+                console.error("Error in request");
+                reject()
+            };
+            xhttp.open("POST", url, true);
+            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            let queryStr = Object.keys(params).reduce(function(a,k){a.push(k+'='+encodeURIComponent(params[k]));return a},[]).join('&');
+            console.log(queryStr);
+            xhttp.send(queryStr);
+        });
+
     }
 }
 
