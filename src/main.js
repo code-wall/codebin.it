2828c46387f38c223a820d6a7aa2f0e7309cbdc7
Alec Hewitt <alec.hewitt9@gmail.com>
2015-12-02 18:57:03 +0000

    Remove commented log statement

--- src/main.js
+++ src/main.js
@@ -52,7 +52,6 @@ class MainDomHandler  {
     }
 
     editorFocused() {
-        //console.log(this.codeEditor.shouldPersist());
         if (!this.codeEditor.shouldPersist()) {
             this.codeEditor.setContent("");
         }
