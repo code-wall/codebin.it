9227796b60db5a756496cdce67f927c1134e151b
Daio-io <dai.williams@bbc.co.uk>
2015-12-09 20:06:29 +0000

    Add token to post request to prevent CSRF - todo refactor to clean up

--- main.go
+++ main.go
@@ -1,24 +1,40 @@
 package main
 
 import (
+	// "fmt"
+	"github.com/code-wall/codebin/Godeps/_workspace/src/github.com/gorilla/csrf"
 	"github.com/code-wall/codebin/Godeps/_workspace/src/github.com/gorilla/mux"
 	"github.com/code-wall/codebin/api"
+	"html/template"
 	"net/http"
 	"os"
 )
 
 func main() {
 	r := mux.NewRouter()
+
+	// Todo: move to suitable place
+	CSRF := csrf.Protect(
+		[]byte("a-32-byte-key-replace-me"),
+		csrf.RequestHeader("Request-Token"),
+		csrf.FieldName("request_token"),
+		csrf.Secure(false), // This should be true in production over https
+	)
+
 	r.HandleFunc("/", indexHandler)
 	r.PathPrefix("/dist/").Handler(createStaticHandler("/dist/", "./dist/"))
 	r.HandleFunc("/save-snippet", api.SaveSnippet)
 	r.HandleFunc("/snippet/{id}", api.GetSnippet)
-	http.ListenAndServe(":"+getPort(), r)
+	http.ListenAndServe(":"+getPort(), CSRF(r))
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	http.ServeFile(w, r, "./views/index.html")
+	t := template.New("index") // todo: move to templating
+	var temps = template.Must(t.ParseFiles("./views/index.html"))
+	data := map[string]interface{}{
+		"token": csrf.Token(r),
+	}
+	temps.ExecuteTemplate(w, "index.html", data)
 }
 
 func createStaticHandler(path string, location string) http.Handler {
