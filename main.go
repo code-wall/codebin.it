20e33f4633191a82a911f30796e1463c889e25bd
Alec Hewitt <alec.hewitt9@gmail.com>
2016-02-20 09:34:45 +1100

    Temporarily remove Content-Security-Policy header in order that Handlebars will work and safari. This should be re added back in when we transition away from Handlebars

--- main.go
+++ main.go
@@ -40,7 +40,6 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("X-Frame-Options", "SAMEORIGIN")
 	w.Header().Set("X-Xss-Protection", "1; mode=block")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
-	w.Header().Set("Content-Security-Policy", "script-src 'self' cdnjs.cloudflare.com")
 
 	temps.ExecuteTemplate(w, "index.html", data)
 }
