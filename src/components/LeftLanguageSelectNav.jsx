c2fc84679c2d799cddb8895119c746e8b0d7a5b7
Alec Hewitt <alec.hewitt9@gmail.com>
2016-03-10 19:43:04 +1100

    Language select working with arrows and pressing enter to highlight a language

--- src/components/LeftLanguageSelectNav.jsx
+++ src/components/LeftLanguageSelectNav.jsx
@@ -6,6 +6,8 @@ import TextField from 'material-ui/lib/text-field';
 
 const LANGUAGES = CodeMirror.modeInfo;
 
+const langBtnRef = "langBtn_";
+
 class LeftLanguageSelectNav extends React.Component {
 
     constructor(props) {
@@ -14,9 +16,14 @@ class LeftLanguageSelectNav extends React.Component {
             languages: LANGUAGES,
             languageSearchValue: ""
         };
+        this.highlightedLan = null;
     }
 
     handleLanguageClick (index, event) {
+        this.selectLanguage(index)
+    }
+
+    selectLanguage(index) {
         this.props.setLanguage(this.state.languages[index].name);
         this.resetState();
         this.props.toggleLanguageSelect(false);
@@ -27,9 +34,17 @@ class LeftLanguageSelectNav extends React.Component {
             languages: LANGUAGES,
             languageSearchValue: ""
         });
+        // Determine if we need to unhighlight a button
+        if (this.highlightedLan !== null && this.refs.hasOwnProperty(langBtnRef + this.highlightedLan)) {
+            this.refs[langBtnRef + this.highlightedLan].refs.listItem.refs.enhancedButton.removeKeyboardFocus();
+        }
+        this.highlightedLan = null;
     }
 
     handleTextFieldChange(event) {
+        console.log("EVENT: ", event);
+        console.log("Native Event: ", event.nativeEvent);
+        console.log("Event Type: ", event.type);
         let value = event.target.value;
         this.setState({languageSearchValue: value});
         value = value.toLowerCase();
@@ -40,8 +55,66 @@ class LeftLanguageSelectNav extends React.Component {
         this.setState({
             languages: languages
         });
-        this.refs.testItem.refs.listItem.refs.enhancedButton.setKeyboardFocus();
+        this.highlightLanguage(0);
     }
+
+    handleKeyDown(event) {
+        console.log("Highlighted Lang: ", this.highlightedLan);
+        switch (event.keyCode) {
+            case 13:
+                // Enter Pressed
+                this.selectLanguage(this.highlightedLan);
+                break;
+            case 38:
+                // Up Arrow pressed
+                this.highlightLanguage(this.highlightedLan === null ? 0 : this.highlightedLan - 1);
+                break;
+            case 40:
+                // Down Arrow pressed
+                this.highlightLanguage(this.highlightedLan === null ? 0 : this.highlightedLan + 1);
+                break;
+        }
+        console.log("Key code", event.keyCode);
+    }
+
+    
+    highlightLanguage(num) {
+        let newHighLighted;
+        // Highlight New
+        if (this.state.languages.length === 0) {
+            // No languages in list
+            newHighLighted = null;
+        } else if (num < 0) {
+            // Gone off top of list
+            let idx = this.state.languages.length - 1;
+            newHighLighted = idx;
+            this.refs[langBtnRef + idx].refs.listItem.refs.enhancedButton.setKeyboardFocus();
+        } else if (this.state.languages.length > num) {
+            // Valid index for language
+            newHighLighted = num;
+            this.refs[langBtnRef + num].refs.listItem.refs.enhancedButton.setKeyboardFocus();
+        } else {
+            // Gone off bottom of list
+            newHighLighted = 0;
+            this.refs[langBtnRef + "0"].refs.listItem.refs.enhancedButton.setKeyboardFocus();
+        }
+        
+        // Unhighlight Previous
+        if (this.highlightedLan !== null && 
+            this.refs.hasOwnProperty(langBtnRef + this.highlightedLan)) {
+            let oldBtn = this.refs[langBtnRef + this.highlightedLan];
+            // Ensure they are not the same button that we have just highlighted
+            if (newHighLighted !== null && this.refs[langBtnRef + newHighLighted] !== oldBtn) {
+                oldBtn.refs.listItem.refs.enhancedButton.removeKeyboardFocus();
+            }
+        }
+        
+        // Set the new state of what is highlighted
+        this.highlightedLan = newHighLighted;
+        console.log("New Highlighed: ", this.highlightedLan);
+    }
+
+
 
     handleMenuChange(open, reason) {
         this.props.toggleLanguageSelect(open);
@@ -61,10 +134,14 @@ class LeftLanguageSelectNav extends React.Component {
                 open={languageSelectOpen}
                 onRequestChange={this.handleMenuChange.bind(this)}
                 >
-                <TextField ref="languageSearch" value={this.state.languageSearchValue} onChange={this.handleTextFieldChange.bind(this)}/>
-                <MenuItem ref="testItem">TEST LANGUAGE 24</MenuItem>
+                <TextField ref="languageSearch" value={this.state.languageSearchValue}
+                           onKeyDown={this.handleKeyDown.bind(this)}
+                           onChange={this.handleTextFieldChange.bind(this)}/>
+                <MenuItem ref="testItem">TEST LANGUAGE 37</MenuItem>
                 {this.state.languages.map((language, i) =>
-                    <MenuItem onTouchTap={this.handleLanguageClick.bind(this, i)} key={i}>{language.name}</MenuItem>
+                    <MenuItem onTouchTap={this.handleLanguageClick.bind(this, i)} key={i} ref={langBtnRef + i}>
+                        {language.name}
+                    </MenuItem>
                 )}
             </LeftNav>
         );
