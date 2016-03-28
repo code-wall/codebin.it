92a8af5f304fe36bbd723189564cc44021098be2
Alec Hewitt <alec.hewitt9@gmail.com>
2016-03-28 13:56:18 +1100

    Add logos to list of languages on left hand side

--- src/components/LeftLanguageSelectNav.jsx
+++ src/components/LeftLanguageSelectNav.jsx
@@ -3,23 +3,44 @@ import LeftNav from 'material-ui/lib/left-nav';
 import Menu from 'material-ui/lib/menus/menu';
 import MenuItem from 'material-ui/lib/menus/menu-item';
 import TextField from 'material-ui/lib/text-field';
+import Download from 'material-ui/lib/svg-icons/file/file-download';
 
-const LANGUAGES = CodeMirror.modeInfo;
+//const LANGUAGES = CodeMirror.modeInfo;
+import {LANGUAGES} from "../constants/languages.js";
 
 const langBtnRef = "langBtn_";
+const defaultIcon = "";
 
 class LeftLanguageSelectNav extends React.Component {
 
     constructor(props) {
         super(props);
+        this.getSvgIcons();
         this.state = {
-            languages: LANGUAGES,
+            languages          : LANGUAGES,
             languageSearchValue: ""
         };
         this.highlightedLan = null;
     }
 
-    handleLanguageClick (index, event) {
+    getSvgIcons() {
+        let parser = new DOMParser();
+        for (let language of LANGUAGES) {
+            if (language.hasOwnProperty("iconKey") && language.hasOwnProperty("iconVersion")) {
+                fetch("https://raw.githubusercontent.com/konpa/devicon/master/icons/"+language.iconKey+"/"+language.iconKey+"-"+language.iconVersion+".svg")
+                    .then(response => response.text())
+                    .then(text => {
+                        let svgDoc = parser.parseFromString(text, "image/svg+xml");
+                        let svg = svgDoc.getElementsByTagName("svg")[0];
+                        language.svgIcon = svg.innerHTML;
+                    });
+            } else {
+                language.svgIcon = "";
+            }
+        }
+    }
+
+    handleLanguageClick(index, event) {
         this.selectLanguage(index)
     }
 
@@ -31,7 +52,7 @@ class LeftLanguageSelectNav extends React.Component {
 
     resetState() {
         this.setState({
-            languages: LANGUAGES,
+            languages          : LANGUAGES,
             languageSearchValue: ""
         });
         // Determine if we need to unhighlight a button
@@ -72,7 +93,7 @@ class LeftLanguageSelectNav extends React.Component {
         }
     }
 
-    
+
     highlightLanguage(num) {
         let newHighLighted;
         // Highlight New
@@ -93,9 +114,9 @@ class LeftLanguageSelectNav extends React.Component {
             newHighLighted = 0;
             this.refs[langBtnRef + "0"].refs.listItem.refs.enhancedButton.setKeyboardFocus();
         }
-        
+
         // Unhighlight Previous
-        if (this.highlightedLan !== null && 
+        if (this.highlightedLan !== null &&
             this.refs.hasOwnProperty(langBtnRef + this.highlightedLan)) {
             let oldBtn = this.refs[langBtnRef + this.highlightedLan];
             // Ensure they are not the same button that we have just highlighted
@@ -103,11 +124,10 @@ class LeftLanguageSelectNav extends React.Component {
                 oldBtn.refs.listItem.refs.enhancedButton.removeKeyboardFocus();
             }
         }
-        
+
         // Set the new state of what is highlighted
         this.highlightedLan = newHighLighted;
     }
-
 
 
     handleMenuChange(open, reason) {
@@ -128,9 +148,27 @@ class LeftLanguageSelectNav extends React.Component {
         }
     }
 
+    setInnerHtml(str) {
+        return { __html: str}
+    }
+    renderMenuItem(language, i) {
+        console.log("SVG: svgIcon: ", language.svgIcon);
+        if (language.hasOwnProperty('svgIcon')) {
+            return (
+                <MenuItem onTouchTap={this.handleLanguageClick.bind(this, i)}
+                          key={i}
+                          ref={langBtnRef + i}
+                          rightIcon={<svg viewBox="0 0 128 128" dangerouslySetInnerHTML={this.setInnerHtml(language.svgIcon)}/>}>
+                    {language.name}
+                </MenuItem>
+            );
+        }
+    }
+
     render() {
         const {languageSelectOpen} = this.props;
         const languages = this.languages;
+        const leftIcon = <i className='devicon-amazonwebservices-original'></i>
         return (
             <LeftNav
                 docked={false}
@@ -143,11 +181,14 @@ class LeftLanguageSelectNav extends React.Component {
                                onKeyDown={this.handleKeyDown.bind(this)}
                                onChange={this.handleTextFieldChange.bind(this)}/>
                 </div>
-                {this.state.languages.map((language, i) =>
-                        <MenuItem onTouchTap={this.handleLanguageClick.bind(this, i)} key={i} ref={langBtnRef + i}>
-                            {language.name}
-                        </MenuItem>
-                )}
+                {this.state.languages.map((language, i) => {
+                    <MenuItem onTouchTap={this.handleLanguageClick.bind(this, i)}
+                              key={i}
+                              ref={langBtnRef + i}
+                              rightIcon={<svg viewBox="0 0 128 128" dangerouslySetInnerHTML={this.setInnerHtml(language.svgIcon)}/>}>
+                        {language.name}
+                    </MenuItem>
+                })}
             </LeftNav>
         );
     }
