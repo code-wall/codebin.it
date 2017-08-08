170f4f8899660058a2e095a2704cfb045b25df08
Alec Hewitt <alec.hewitt9@gmail.com>
2017-08-09 09:12:54 +1000

    Embed application (#58)
    * Add embed application to be used for people to iframe a snippet
    
    * Imporve look of embed application, fix bug with everything saved to the dist html not the source
    
    * replact 'let' with 'var' in order to make work on safari

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 	"github.com/gorilla/mux"
 	"net/http"
 	"text/template"
+	"encoding/json"
 )
 
 var conf = GetConfig()
-var temps *template.Template
+var mainTemplate *template.Template
+var embedTemplate *template.Template
 
 const productionAnalytics = "UA-75805487-1"
 const stagingAnalytics = "UA-75805487-2"
@@ -20,7 +22,8 @@ func main() {
 	// Set template and the delimeters
 	t := template.New("index")
 	t.Delims("<<<", ">>>")
-	temps = template.Must(t.ParseFiles("./views/index.html"))
+	mainTemplate = template.Must(t.ParseFiles("./views/index.html"))
+	embedTemplate = template.Must(t.ParseFiles("./views/embed.html"))
 
 	CSRF := csrf.Protect(
 		[]byte(conf.CSRFKey),
@@ -29,6 +32,7 @@ func main() {
 		csrf.Secure(!conf.Debug),
 	)
 
+	r.HandleFunc("/embed/{id}", embedHandler)
 	r.HandleFunc("/", indexHandler)
 	r.PathPrefix("/dist/").Handler(createStaticHandler("/dist/", "./dist/"))
 	r.HandleFunc("/save", api.SaveSnippet)
@@ -51,7 +55,42 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	// Temp disable until serving site over https
 	//	w.Header().Set("Content-Security-Policy", "script-src 'self' cdnjs.cloudflare.com")
 
-	temps.ExecuteTemplate(w, "index.html", data)
+	mainTemplate.ExecuteTemplate(w, "index.html", data)
+}
+
+func embedHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	snippet, err := api.GetService().GetSnippetByID(id)
+	if err != nil {
+		http.Error(w, "Unable to find snippet", http.StatusBadRequest)
+	} else {
+		// Available Query Variables:
+		//     theme - { light | dark }
+		//     readonly - { true | false }
+		themeName := "solarized dark"
+		bgColor := "#002B36"
+		if r.URL.Query().Get("theme") == "light" {
+			themeName = "default"
+			bgColor = "#ffffff"
+		}
+		readonly := "true"
+		if r.URL.Query().Get("readonly") == "false" {
+			readonly = "false"
+		}
+		code, _ := json.Marshal(snippet.Snippet)
+		data := map[string]interface{}{
+			"code": string(code),
+			"language": snippet.Language,
+			"themeName": themeName,
+			"bgColor": bgColor,
+			"readonly": readonly,
+			"snippetId": id,
+		}
+		embedTemplate.ExecuteTemplate(w, "embed.html", data)	
+	}
+	
+
 }
 
 func createStaticHandler(path string, location string) http.Handler {
