6a7e3ed253af90fc1dcabc32c8f9c60d0549499c
Daio-io <dai.williams@bbc.co.uk>
2017-09-02 00:53:49 +0100

    Use material textfield for save dialog link text

--- src/components/SaveDialog.jsx
+++ src/components/SaveDialog.jsx
@@ -3,6 +3,7 @@ import Dialog from 'material-ui/lib/dialog';
 import FlatButton from 'material-ui/lib/flat-button';
 import RaisedButton from 'material-ui/lib/raised-button';
 import CircularProgress from 'material-ui/lib/circular-progress';
+import TextField from 'material-ui/lib/text-field';
 import Theme from '../constants/theme';
 
 class SaveDialog extends React.Component {
@@ -47,21 +48,23 @@ class SaveDialog extends React.Component {
         } 
         else {
             return (
-                <p>Saved link: 
-                    <input ref="snippet-link" value={this.props.snippet.currentLink} />
+                <div> <p>Saved link:</p> 
+                    <TextField 
+                    id="snippet-link" 
+                    value={this.props.snippet.currentLink} 
+                    readonly/>
 
                     <button onClick={() => this.copyLink()} aria-label="Copy to clipboard" data-copied-hint="Copied!" type="button">
                         <svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path fill-rule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"></path></svg>
                     </button>
-                </p>
+                </div>
             );
         }
     }
 
     copyLink() {
         console.log('Copying LINK!');
-        this.refs['snippet-link'].select();
-
+        document.getElementById("snippet-link").select()
         try {
             var successful = document.execCommand('copy');
             var msg = successful ? 'successful' : 'unsuccessful';
